2a0cc1d5ed0fd79a21206baf79b2a0cd30ad14cb
ahsanulhaq <ulhaq.ahsan777@gmail.com>
2023-02-05 04:09:51 +0500

    PanaverseDaoWeb to new repo from nextjs-learning

---
+++ pananverse-dao-web/src/app/layout.tsx
@@ -0,0 +1,14 @@
+"use client"
+import ChakraWrapper from "@/components/chakraWrapper"
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head />
+      <body><ChakraWrapper>{children}</ChakraWrapper></body>
+    </html>
+  )
+}
---
+++ pananverse-dao-web/src/app/page.tsx
@@ -0,0 +1,13 @@
+
+import Header from '../components/header'
+import Footer from '../components/footer'
+import MainPage from '../app/home'
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <MainPage />
+      <Footer />
+    </div>
+  )
+}
